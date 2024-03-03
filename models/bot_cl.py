978e4e86b2aa673d2f8ec359ad5ed16a25c25672
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-03 15:00:36 +1100

    botcl wip

--- models/bot_cl.py
+++ models/bot_cl.py
@@ -1,21 +1,24 @@
 import torch
+import numpy as np
 from torch import nn
+from PIL import Image
 from timm import create_model
 import torch.nn.functional as F
+
 from .positional_encoding import PositionalEncoding2D
 
 
 class ScouterAttention(nn.Module):
-    def __init__(self, dim, num_concept, iters=3, eps=1e-8, vis=False, power=1, to_k_layer=3):
+    def __init__(self, args, dim, num_concept, iters=3, eps=1e-8, vis=False, power=1, to_k_layer=3):
         super().__init__()
+        self.args = args
         self.num_slots = num_concept
-        self.iters = iters
         self.eps = eps
-        self.scale = dim ** (-0.5)
+        self.scale = dim ** -0.5
 
         # random seed init
-        slots_mu = nn.Parameter(torch.randn(1, 1, dim))
-        slots_sigma = nn.Parameter(torch.abs(torch.randn(1, 1, dim)))
+        slots_mu = torch.randn(1, 1, dim)
+        slots_sigma = torch.abs(torch.randn(1, 1, dim))
         mu = slots_mu.expand(1, self.num_slots, -1)
         sigma = slots_sigma.expand(1, self.num_slots, -1)
         self.initial_slots = nn.Parameter(torch.normal(mu, sigma))
@@ -32,39 +35,80 @@ class ScouterAttention(nn.Module):
         self.vis = vis
         self.power = power
 
-    def forward(self, inputs_pe, inputs):
+    def forward(self, inputs_pe, inputs, weight=None, things=None):
         b, n, d = inputs_pe.shape
         slots = self.initial_slots.expand(b, -1, -1)
         k, v = self.to_k(inputs_pe), inputs_pe
-        print('slot.k.shape:', k.shape)
         for _ in range(self.iters):
             q = slots
-            print('slot.q.shape:', q.shape)
 
             dots = torch.einsum('bid,bjd->bij', q, k) * self.scale
-            dots = torch.div(dots, dots.sum(2).expand_as(dots.permute([2, 0, 1])).permute([1, 2, 0])) * \
-                   dots.sum(2).sum(1).expand_as(dots.permute([1, 2, 0])).permute([2, 0, 1])
+            scale_fct = dots.sum(2).sum(1)[:, None, None].expand_as(dots)
+            dots = torch.div(dots, dots.sum(2).unsqueeze(-1).expand_as(dots)) * scale_fct
             attn = torch.sigmoid(dots)
-            print('slot.dots.shape:', dots.shape)
-            print('slot.attn.shape:', attn.shape)
-
-
-            # print(torch.max(attn))
-            # dsfds()
 
             attn2 = attn / (attn.sum(dim=-1, keepdim=True) + self.eps)
-            print('slot.attn2.shape:', attn2.shape)
             updates = torch.einsum('bjd,bij->bid', inputs, attn2)
-            print('slot.updates.shape:', updates.shape)
-            break
+        if self.vis:
+            slots_vis_raw = attn.clone()
+            vis(slots_vis_raw, "vis", self.args.feature_size, weight, things)
         return updates, attn
 
 
-class MainModel(nn.Module):
+def vis(slots_vis_raw, loc, size, weight=None, things=None):
+    b = slots_vis_raw.size()[0]
+    for i in range(b):
+        slots_vis = slots_vis_raw[i]
+        if weight is not None:
+            Nos = weight[1]
+            weight = weight[0]
+            slots_vis = slots_vis * weight.unsqueeze(-1)
+
+            overall = slots_vis.sum(0)
+            overall = (((overall - overall.min()) / (overall.max() - overall.min())) * 255.).reshape((int(size), int(size)))
+            overall = (overall.cpu().detach().numpy()).astype(np.uint8)
+            overall = Image.fromarray(overall, mode='L').resize([224, 224], resample=Image.BILINEAR)
+            overall.save(f'{loc}/overall_{Nos}.png')
+
+        else:
+            slots_vis = ((slots_vis - slots_vis.min()) / (slots_vis.max() - slots_vis.min()) * 255.).reshape(
+                slots_vis.shape[:1] + (int(size), int(size)))
+
+            slots_vis = (slots_vis.cpu().detach().numpy()).astype(np.uint8)
+            for id, image in enumerate(slots_vis):
+
+                image = Image.fromarray(image, mode='L').resize([224, 224], resample=Image.BILINEAR)
+                if things is not None:
+                    order, category, cpt_num = things
+                    loc2 = f"vis_pp/cpt{cpt_num}/"
+                    if id == cpt_num:
+                        image.save(loc2 + f'mask_{order}_{category}.png')
+                        break
+                    else:
+                        continue
+                image.save(f'{loc}/{i}_slot_{id:d}.png')
+
+
+class ResNetClassifier(nn.Module):
+    def __init__(self, model_name: str='resnet18', dim=512, num_classes=200):
+        super(ResNetClassifier, self).__init__()
+        self.resnet = create_model(model_name, pretrained=True)
+        self.fc = nn.Linear(dim, num_classes)
+        self.dropout_rate = 0
+
+    def forward(self, x):
+        x = self.resnet.forward_features(x)
+        x = F.adaptive_max_pool2d(x, 1).squeeze(-1).squeeze(-1)
+        if self.dropout_rate > 0:
+            x = F.dropout(x, p=self.drop_rate, training=self.training)
+        x = self.fc(x)
+        return x
+
+
+class BotCl(nn.Module):
     def __init__(self, args, vis=False):
-        super(MainModel, self).__init__()
+        super(BotCl, self).__init__()
         self.args = args
-        self.pre_train = args.pre_train
         if "18" not in args.base_model:
             self.num_features = 2048
         else:
@@ -78,46 +122,39 @@ class MainModel(nn.Module):
         self.activation = nn.Tanh()
         self.vis = vis
 
-        if not self.pre_train:
-            self.conv1x1 = nn.Conv2d(self.num_features, hidden_dim, kernel_size=(1, 1), stride=(1, 1))
-            self.norm = nn.BatchNorm2d(hidden_dim)
-            self.position_emb = PositionalEncoding2D(hidden_dim)
-            self.slots = ScouterAttention(hidden_dim, num_concepts, vis=self.vis)
-            self.scale = 1
-            self.cls = torch.nn.Linear(num_concepts, num_classes)
-        else:
-            self.fc = nn.Linear(self.num_features, num_classes)
-            self.drop_rate = 0
+        self.scale = 1
+        self.conv1x1 = nn.Conv2d(self.num_features, hidden_dim,
+                                 kernel_size=(1, 1), stride=(1, 1))
+        self.norm = nn.BatchNorm2d(hidden_dim)
+        self.position_emb = PositionalEncoding2D(hidden_dim)
+        self.slot_attention = ScouterAttention(hidden_dim, num_concepts, vis=self.vis)
+        self.classifier = torch.nn.Linear(num_concepts, num_classes)
+
 
     def forward(self, x):
         x = self.back_bone.forward_features(x)
-        features = x
-        # x = x.view(x.size(0), self.num_features, self.feature_size, self.feature_size)
 
-        if not self.pre_train:
-            x = self.conv1x1(x)
-            x = self.norm(x)
-            x = torch.relu(x)
-            pe = self.position_emb(x)
-            x_pe = x + pe
+        x = self.conv1x1(x)
+        x = self.norm(x)
+        x = torch.relu(x)
+        pe = self.position_emb(x)
+        x_pe = x + pe
 
-            b, c, h ,w = x.shape
-            x = x.reshape((b, c, -1)).permute((0, 2, 1)) # shape: b, c, h*w
-            x_pe = x_pe.reshape((b, c, -1)).permute((0, 2, 1)) # shape: b, c, h*w
-            print('x.shape:', x.shape, 'x_pe.shape:', x_pe.shape)
+        b, c = x.shape[:2]
+        x = x.reshape((b, c, -1)).permute((0, 2, 1)) # shape: [b, c, h*w]
+        x_pe = x_pe.reshape((b, c, -1)).permute((0, 2, 1)) # shape: [b, c, h*w]
 
-            updates, attn = self.slots(x_pe, x)
-            if self.args.cpt_activation == "att":
-                cpt_activation = attn
-            else:
-                cpt_activation = updates
-            attn_cls = self.scale * torch.sum(cpt_activation, dim=-1)
-            cpt = self.activation(attn_cls)
-            cls = self.cls(cpt)
-            return (cpt - 0.5) * 2, cls, attn, updates
+        # concepts shape: [b, num_concepts, dim];
+        # attention_scores shape: [b, num_concepts, num_patches]
+        concepts, attention_scores = self.slot_attention(x_pe, x)
+        if self.args.cpt_activation == "att":
+            cpt_activation = attention_scores
         else:
-            x = F.adaptive_max_pool2d(x, 1).squeeze(-1).squeeze(-1)
-            if self.drop_rate > 0:
-                x = F.dropout(x, p=self.drop_rate, training=self.training)
-            x = self.fc(x)
-            return x, features
+            cpt_activation = concepts
+        attn_cls = self.scale * torch.sum(cpt_activation, dim=-1)
+
+        concept_logits = self.activation(attn_cls)
+        class_logits = self.classifier(concept_logits)
+        concept_logits_hat = (concept_logits - 0.5) * 2
+
+        return concept_logits_hat, class_logits, attention_scores, concepts
